ecdf6f4d7d9187ba8434496ceae8e77821b0bf0d
carloserig <carlos.erig@gmail.com>
2026-02-27 09:42:37 -0300

    Estrutura de pastas

--- fundamentos/variaveis/aula02/aula02.go
+++ fundamentos/variaveis/aula02/aula02.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	//Variáveis
 	//string
-	//bool
 	//int / int8 / int16 / int32 / int64
+	// 8 bits → 2ˆ8 = 256 valores possíveis 128 negativos 128 positivos (-128 a 127)
 	//uint / uint8 / uint16 / uint32 / uint64
 	//float / float32 / float64
+	//bool
 	//var num int16 = 32767
 	//fmt.Println(num)
 	fmt.Println("Variáveis")
@@ -20,7 +22,7 @@ func main() {
 	//nome := "Carlos"
 	var sobrenome = "Erig"
 	fmt.Println("Variáveis do tipo float")
-	var numero = 40.20
+	var numero = 40.2045689
 	fmt.Println(nome + " " + sobrenome)
 	fmt.Printf("Número: %.2f\n", numero)
 	nome = "Teste"
