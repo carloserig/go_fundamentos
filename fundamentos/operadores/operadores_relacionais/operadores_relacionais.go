7cf88e795ff0ab44cf4cc78d83fa4b30c0ccaa40
carloserig <carlos.erig@gmail.com>
2026-03-06 09:16:32 -0300

    avançando com go

---
+++ fundamentos/operadores/operadores_relacionais/operadores_relacionais.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Operadores relacionais")
+	/*
+		==	igual
+		!=	diferente
+		>	maior
+		<	menor
+		>=	maior ou igual
+		<=	menor ou igual
+	*/
+
+}
